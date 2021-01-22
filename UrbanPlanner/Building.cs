a242a86ffa6ab93a50fb225d2a1c476aca34f869
Sam Mudick <smudick@gmail.com>
2021-01-21 21:31:31 -0600

    urban planner part 1

---
+++ UrbanPlanner/Building.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UrbanPlanner
+{
+    class Building
+    {
+        private string _designer { get; set; }
+        private DateTime _dateContructed { get; set; }
+        private string _address { get; set; }
+        private string _owner { get; set; }
+        public int Stories { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public double Volume => Width * Depth * (3 * Stories);
+
+        public Building(string address)
+        {
+            _address = address;
+        }
+
+        public void Construct()
+        {
+            _dateContructed = DateTime.Now;
+        }
+        public void Purchase(string purchaser)
+        {
+            _owner = purchaser;
+        }
+        public void Design(string designer)
+        {
+            _designer = designer;
+        }
+        public void Write()
+        {
+            Console.WriteLine($"{_address}-----------");
+            Console.WriteLine($"Designed by {_designer}");
+            Console.WriteLine($"Constructed on {_dateContructed}");
+            Console.WriteLine($"Owned by {_owner}");
+            Console.WriteLine($"{Volume} cubic meters of space");
+        }
+
+   
+    }
+}
---
+++ UrbanPlanner/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace UrbanPlanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Building FiveOneTwoEight = new Building("512 8th Avenue");
+            FiveOneTwoEight.Width = 50;
+            FiveOneTwoEight.Depth = 40;
+            FiveOneTwoEight.Stories = 15;
+            FiveOneTwoEight.Design("Frank Lloyd Wright");
+            FiveOneTwoEight.Construct();
+            FiveOneTwoEight.Purchase("Sam Mudick");
+            FiveOneTwoEight.Write();
+
+            Building MyHouse = new Building("3713 Burrus Street");
+            MyHouse.Width = 6.67;
+            MyHouse.Depth = 6.67;
+            MyHouse.Stories = 2;
+            MyHouse.Design("Some dude in the 40s");
+            MyHouse.Construct();
+            MyHouse.Purchase("Sam Mudick");
+            MyHouse.Write();
+
+        }
+    }
+}
